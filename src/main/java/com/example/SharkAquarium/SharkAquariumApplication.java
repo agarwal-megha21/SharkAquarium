d3db122a24424cff169433faa74ad2dedbadf8dd
agarwal-megha21 <megha.agarwal.cse19@itbhu.ac.in>
2022-02-15 23:39:49 +0530

    Setup

---
+++ src/main/java/com/example/SharkAquarium/SharkAquariumApplication.java
@@ -0,0 +1,13 @@
+package com.example.SharkAquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkAquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkAquariumApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SharkAquarium/model/user.java
@@ -0,0 +1,5 @@
+package com.example.SharkAquarium.model;
+
+public class user {
+	
+}
