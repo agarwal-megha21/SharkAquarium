86d0871dcc87ead179e289a3a55edb2d50ce74be
agarwal-megha21 <megha.agarwal.cse19@itbhu.ac.in>
2022-02-16 23:35:22 +0530

    Basic Setup

--- src/main/java/com/example/SharkAquarium/controller/AdminController.java
+++ src/main/java/com/example/SharkAquarium/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.example.SharkAquarium.controller;
 
+import com.example.SharkAquarium.model.user;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,14 @@ public class AdminController {
     
     @GetMapping("/")
     public String test() {
-        return "Helllo Everyone!";
+        return "Hello Everyone!";
     }
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        user user = new user();
+        model.addAttribute("userForm", user);
+        return "register";
+    }
+
 }
