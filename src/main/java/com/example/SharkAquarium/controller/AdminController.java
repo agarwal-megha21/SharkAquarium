6b0aebb2c1dc76d322b016b57dc53460014e2c2a
M-mitu12 <mayu.nitp@gmail.com>
2022-02-18 09:47:25 +0530

    modified register.html__render changes

--- src/main/java/com/example/SharkAquarium/controller/AdminController.java
+++ src/main/java/com/example/SharkAquarium/controller/AdminController.java
@@ -2,13 +2,13 @@ package com.example.SharkAquarium.controller;
 
 import com.example.SharkAquarium.model.user;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AdminController {
-    
+
     @GetMapping("/")
     public String test() {
         return "Hello Everyone!";
