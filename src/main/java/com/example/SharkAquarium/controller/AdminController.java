f9dbe865cb27b18d4bacb3b35da16eedfa54e0d9
M-mitu12 <mayu.nitp@gmail.com>
2022-02-18 15:23:36 +0530

    added html files

--- src/main/java/com/example/SharkAquarium/controller/AdminController.java
+++ src/main/java/com/example/SharkAquarium/controller/AdminController.java
@@ -11,7 +11,7 @@ public class AdminController {
 
     @GetMapping("/")
     public String test() {
-        return "Hello Everyone!";
+        return "index";
     }
 
     @GetMapping("/register")
