4fc5395ce693a4f8d1ce05ec014274984401b6a3
M-mitu12 <mayu.nitp@gmail.com>
2022-02-22 22:59:17 +0530

    added gitignore

--- src/main/java/com/example/SharkAquarium/model/user.java
+++ src/main/java/com/example/SharkAquarium/model/user.java
@@ -1,7 +1,7 @@
 package com.example.SharkAquarium.model;
 
 public class user {
-    
+
     private String email;
     private String password;
     private String passwordConfirm;
@@ -10,26 +10,33 @@ public class user {
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getPasswordConfirm() {
         return passwordConfirm;
     }
+
     public void setPasswordConfirm(String passwordConfirm) {
         this.passwordConfirm = passwordConfirm;
     }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
-	
+
 }
